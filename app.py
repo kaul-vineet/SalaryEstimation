196f687e3e68702ba4ad85ff96898c84227a08bd
Vineet Kaul <vkaul@internal.salesforce.com>
2019-09-05 19:20:50 +0530

    final commit

--- app.py
+++ app.py
@@ -57,9 +57,8 @@ from flask import Flask, render_template, request
     
 app=Flask(__name__)
 @app.route('/')
-@app.route('/index')
 def index():
-    return flask.render_template('index.html')
+    return flask.render_template('index-ui.html')
 
 def ValuePredictor(to_predict_list):
     to_predict = np.array(to_predict_list).reshape(1,12)
@@ -67,19 +66,26 @@ def ValuePredictor(to_predict_list):
     result = loaded_model.predict(to_predict)
     return result[0]
 
+@app.route('/apidoc')
+def apidoc():
+    return flask.render_template('index-api.html')
+
 @app.route('/result',methods = ['POST'])
 def result():
-    if request.method == 'POST':
-        to_predict_list = request.form.to_dict()
-        to_predict_list=list(to_predict_list.values())
-        to_predict_list = list(map(int, to_predict_list))
-        result = ValuePredictor(to_predict_list)
-        if int(result)==1:
-            prediction='Income more than 50K'
-        else:
-            prediction='Income less that 50K'
-        return render_template("result.html",prediction=to_predict_list)
-
+    try:
+        if request.method == 'POST':
+            to_predict_list = request.form.to_dict()
+            to_predict_list=list(to_predict_list.values())
+            to_predict_list = list(map(int, to_predict_list))
+            result = ValuePredictor(to_predict_list)
+            if int(result)==1:
+                prediction='Income more than 50K'
+            else:
+                prediction='Income less that 50K'
+            return render_template("result.html",prediction=prediction)
+    except Exception as e:
+	    return render_template("error.html", error = str(e))
+		
 @app.route('/predictsalary',methods=['POST'])
 def SalaryPrediction():
     
